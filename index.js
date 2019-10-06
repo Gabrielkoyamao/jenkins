af8ed09c384205fcbb4abb98c625bb38aef73fd6
gabriel.koyama <gabriel.koyama44@gmail.com>
2019-10-06 15:02:17 -0300

    adding env variables

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+require('dotenv').config()
 var app = express();
+
+
 app.get('/', function (req, res) {
- res.send("Heello from semaphore!!!");
+ res.send("Heello from semaphore " + process.env.DB_HOST + "!!!");
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
