c2d71b0156254ba469f4fb829a6a502750a110fa
gabriel.koyama <gabriel.koyama44@gmail.com>
2019-10-06 14:54:24 -0300

    change deploy sempaho && git push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
- res.send("Heelo from semaphore!!!");
+ res.send("Heello from semaphore!!!");
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
