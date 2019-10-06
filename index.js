cde9c9cdbdcfd7981f6973c1530460614ebcfd30
gabriel.koyama <gabriel.koyama44@gmail.com>
2019-10-06 16:24:35 -0300

    e

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+require('dotenv').config();
 var app = express();
 
 app.get('/', function (req, res) {
- console.log('TESTE LOG, calling index')
- res.send("HELLO MUDEI!!!");
+    console.log('process env == ', process.env)
+ res.send("HELLO MUDEI!!!" + process.env.DB_HOST);
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
